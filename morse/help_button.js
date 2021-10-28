21bda9f1b72e2ae2895d830d6f8f281955d20a01
Alan Zhihao Lu <45406389+AlanZhihaoLu@users.noreply.github.com>
2021-10-28 12:53:32 -0700

    help button less buggy, removed apotential double tap at help button

--- morse/help_button.js
+++ morse/help_button.js
@@ -1,25 +1,29 @@
 var help_button = document.body.querySelector('.help-button');
+var help_content = document.body.querySelector('.help-content');
 var close_help = document.querySelector("#close-help");
 
 help_button.addEventListener('mousedown', function (e) {
     e.stopPropagation();
     modal.style.display = "block";
+    help_content.style.display = "block";
 }, true)
 
 help_button.addEventListener('touchstart', function (e) {
     e.stopPropagation();
     modal.style.display = "block";
+    help_content.style.display = "block";
 }, true)
 
-close_help.addEventListener('mousedown', function (e) {
+close_help.addEventListener('click', function(e) {
     e.stopPropagation();
-    modal.style.display = "none";
+    help_content.style.display="none";
+    modal.style.display="none";
 }, true)
 
-close_help.addEventListener('touchstart', function (e) {
+close_help.addEventListener('touchend', function(e) {
     e.stopPropagation();
-    modal.style.display = "none";
+    help_content.style.display="none";
+    modal.style.display="none";
 }, true)
 
-absorbEvents(close_help);
 absorbEvents(help_button);
