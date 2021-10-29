f66380ae10842e0aa42b1cdfa3428be3aa8a0293
Alan Zhihao Lu <45406389+AlanZhihaoLu@users.noreply.github.com>
2021-10-28 23:14:22 -0700

    added volume slider

---
+++ morse/options_button.js
@@ -0,0 +1,29 @@
+var options_button = document.body.querySelector('.options-button');
+var options_content = document.body.querySelector('.options-content');
+var close_options = document.querySelector("#close-options");
+
+options_button.addEventListener('mousedown', function (e) {
+    e.stopPropagation();
+    modal.style.display = "block";
+    options_content.style.display = "block";
+}, true)
+
+options_button.addEventListener('touchstart', function (e) {
+    e.stopPropagation();
+    modal.style.display = "block";
+    options_content.style.display = "block";
+}, true)
+
+close_options.addEventListener('click', function(e) {
+    e.stopPropagation();
+    options_content.style.display="none";
+    modal.style.display="none";
+}, true)
+
+close_options.addEventListener('touchend', function(e) {
+    e.stopPropagation();
+    options_content.style.display="none";
+    modal.style.display="none";
+}, true)
+
+absorbEvents(options_button);
