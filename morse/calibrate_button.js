c0b25dfcc22a4cb399333188638b1435177fcdc4
Alan Zhihao Lu <45406389+AlanZhihaoLu@users.noreply.github.com>
2021-10-31 00:39:38 -0700

    disabled regular keypress listeners when calibration menu is open

--- morse/calibrate_button.js
+++ morse/calibrate_button.js
@@ -4,6 +4,10 @@ var close_cal = document.querySelector("#close-cal");
 
 cal_button.addEventListener('mousedown', function (e) {
     e.stopPropagation();
+    // remove keypress event listeners to prevent default action
+    document.body.removeEventListener("keydown", handle_keydown);
+    document.body.removeEventListener("keyup", handle_keyup);
+    keypress_interpreter_inactive = true;
     modal.style.display = "block";
     cal_content.style.display = "block";
 }, true)
@@ -22,6 +26,9 @@ close_cal.addEventListener('mouseup', function(e) {
     e.stopPropagation();
     cal_content.style.display="none";
     modal.style.display="none";
+    // re-add keypress default event listeners 
+    document.body.addEventListener('keydown', handle_keydown)
+    document.body.addEventListener('keyup', handle_keyup)
 }, true)
 
 close_cal.addEventListener('touchend', function(e) {
@@ -35,52 +42,40 @@ absorbEvents(cal_button);
 var ready_down = true;
 var t0;
 var dur;
+var dur_list = [];
 
-// cal_content.addEventListener('mousedown', function (e) {
-//     e.preventDefault();
-//     if (ready_down) {
-//         t0 = e.timeStamp;
-//         if (timeoutID !== undefined) {
-//             clearTimeout(timeoutID);
-//         }
-//         audioObj.play()
-//         ready_down = false;
-//     }
-// })
+hdoots = document.querySelectorAll('.hcal > span.visFeedback');
+pdoots = document.querySelectorAll('.pcal > span.visFeedback');
 
-// cal_content.addEventListener('mouseup', function(e) {
-//     e.preventDefault();
-//     if (t0 !== undefined) {
-//         dur = e.timeStamp - t0;
-//         console.log(dur)
-//         audioObj.pause()
-//         audioObj.currentTime = 0;
-//         ready_down = true;
-//     }
-// })
+function handle_mousedown_calibrate(e) {
+    e.preventDefault();
+    if (ready_down) {
+        t0 = e.timeStamp;
+        if (timeoutID !== undefined) {
+            clearTimeout(timeoutID);
+        }
+        if (audioReady) {
+            audioObj.play();
+        }
+        ready_down = false;
+    }
+}
+function handle_mouseup_calibrate(e) {
+    e.preventDefault();
+    if (t0 !== undefined) {
+        dur = e.timeStamp - t0;
+        dur_list.push(dur);
+        if (audioReady) {
+            audioObj.pause();
+            audioObj.currentTime = 0;
+        }
+        ready_down = true;
+    }
+    if (dur_list.length === 4) {
+        cal_content.removeEventListener('mousedown', handle_mousedown_calibrate);
+        cal_content.removeEventListener('mouseup', handle_mouseup_calibrate);
+    }
+}
 
-
-// cal_content.addEventListener('keydown', function (e) {
-//     e.preventDefault();
-//     e.stopImmediatePropagation();
-//     if (e.keyCode === 32 && ready_down) {
-//         t0 = e.timeStamp;
-//         if (timeoutID !== undefined) {
-//             clearTimeout(timeoutID);
-//         }
-//         audioObj.play()
-//         ready_down = false;
-//     }
-// }, true)
-
-// cal_content.addEventListener('keyup', function(e) {
-//     e.preventDefault();
-//     e.stopImmediatePropagation();
-//     if (e.keyCode === 32 && t0 !== undefined) {
-//         dur = e.timeStamp - t0;
-//         console.log(dur)
-//         audioObj.pause()
-//         audioObj.currentTime = 0;
-//         ready_down = true;
-//     }
-// }, true)
+cal_content.addEventListener('mousedown', handle_mousedown_calibrate);
+cal_content.addEventListener('mouseup', handle_mouseup_calibrate);
