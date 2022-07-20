8bc126bab985db85c9ba23278561ae121ce6dc08
Alan Zhihao Lu <45406389+AlanZhihaoLu@users.noreply.github.com>
2022-07-20 10:06:47 -0400

    added in directory specification CLI

--- debugserver/server.js
+++ debugserver/server.js
@@ -3,18 +3,30 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-// Specify file path relative to current directory
-const filepath = '/../morse'
+var srcfilesdir;
 
-try {
-    app.use(express.static(path.join(__dirname + filepath)))
-    router.get('/', (req, res) => {
-        res.sendFile('/index.html')
-    });
-    app.use('/', router);
-    app.listen(process.env.PORT || 3000, ()=>{
-        console.log(`app is running on port 3000`)
-    });
-} catch(err) {
-    console.log("this is a server error:", err)
-}
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question(`Enter Directory: `, server_loc => {
+
+    // Specify file path relative to current directory
+    const filepath = `/../${server_loc}`
+
+    try {
+        app.use(express.static(path.join(__dirname + filepath)))
+        router.get('/', (req, res) => {
+            res.sendFile('/index.html')
+        });
+        app.use('/', router);
+        app.listen(process.env.PORT || 3000, ()=>{
+            console.log(`app is running on port 3000`)
+        });
+    } catch(err) {
+        console.log("this is a server error:", err)
+    }
+
+    readline.close();
+});
